6c1395b32a88c6076a782b0b3b728c4fde20a4a6
Princekotadiya007 <princuuu77@gmail.com>
2023-03-29 09:11:06 +0530

    [prince] ProductReviewManagemnet

---
+++ ProductReviewManagmentUsingLINQ/ProductReviewManagmentUsingLINQ/ProductReviewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagmentUsingLINQ
+{
+    public class ProductReviewModel
+    {
+        public int PeoductId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }          
+        public string IsLike { get; set; }
+    }
+}
---
+++ ProductReviewManagmentUsingLINQ/ProductReviewManagmentUsingLINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagmentUsingLINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProductReviewModel> list = new List<ProductReviewModel>();
+
+            new ProductReviewModel()(ProductId=1, UserId = 10, Rating = 4, ProductReviewModel = "good", IsLike = true);
+        }
+    }
+}
